a7b74a2b1884acab734777132eabc3f4cd33572a
liketed <redrocketed@gmail.com>
2016-10-17 10:29:46 +0200

    Commit my packer files for centos7.2

---
+++ README.md
@@ -0,0 +1,3 @@
+I still need custom vagrant boxes, and while everyone else seems to be abonding their packer files for docker files I still need to build some custom boxes.
+
+Up to date centos7, puppet-agent 1.5, and vbox guest additions.
---
+++ scripts/virtualbox.sh
@@ -0,0 +1,12 @@
+# Installing packages separately to debug something
+yum clean all
+yum update -y
+yum install -y gcc
+yum install -y kernel-devel
+yum install -y perl  
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+cd /tmp
+mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run --nox11
+umount /mnt
+rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
