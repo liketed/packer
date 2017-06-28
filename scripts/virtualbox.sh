d910cead36e5b1ac88e2a6c2035610aaf6f7529d
liketed <redrocketed@gmail.com>
2017-06-28 21:14:36 +0200

    Fedora's kickstart file seems to have changed a bit. I hope these changes are compatible with CentOS also.

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -3,6 +3,9 @@ yum clean all
 yum update -y
 yum install -y gcc
 yum install -y perl  
+yum install -y bzip2
+yum install -y dkms
+yum install -y kernel-devel
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 cd /tmp
 mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
