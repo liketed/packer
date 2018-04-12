6a36220c8cf1c41a02a58b43a9b45a4d2656ee19
liketed <redrocketed@gmail.com>
2018-04-12 17:31:34 +0100

    Add some quick support for vmware provider for vagrant, specifically vmhgfs mountsd. WorksForMe ¯\_(ツ)_/¯

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,1 +1,3 @@
-yum -y install vim-enhanced wget
+yum -y install vim-enhanced
+yum -y install wget
+yum -y install psmisc
--- scripts/vmware.sh
+++ scripts/vmware.sh
@@ -1,1 +1,13 @@
+yum -y install epel-release
 yum -y install open-vm-tools
+yum -y install perl gcc fuse make kernel-devel net-tools policycoreutils-python
+mkdir -p /tmp/vmware /tmp/vmware-archive
+mount -o loop /home/vagrant/linux.iso /tmp/vmware
+TOOLS_PATH="`ls /tmp/vmware/VMwareTools-*.tar.gz`"
+tar xzf ${TOOLS_PATH} -C /tmp/vmware-archive
+/tmp/vmware-archive/vmware-tools-distrib/vmware-install.pl --force-install --default
+umount /tmp/vmware
+rm -rf /tmp/vmware /tmp/vmware-archive
+rm -f /home/vagrant/*.iso
+rm -f /etc/udev/rules.d/70-persistent-net.rules
+rm -f /etc/sysconfig/network-scripts/ifcfg-ens33
